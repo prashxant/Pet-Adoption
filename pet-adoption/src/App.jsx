7723e42e5466434b75247e7fa3ab1548d88cdb06
prashxant <notxdead0@gmail.com>
2025-04-04 19:57:51 +0530

    i cnt code

--- pet-adoption/src/App.jsx
+++ pet-adoption/src/App.jsx
@@ -1,13 +1,27 @@
 
-import './App.css'
+
 
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+        Pet name 
+        <input style={{width:"10vw"}} type="text" />
+        Pet type
+        <input style={{width:"10vw"}} type="text" />
+        Breed
+        <input style={{width:"10vw"}} type="text" />
+        Your Name
+        <input style={{width:"10vw"}} type="text" />
+        Email
+        <input style={{width:"10vw"}} type="text" />
+        Phone
+        <input style={{width:"10vw"}} type="text" />
+        <button onClick={()=> { alert("Submited")}} style={{width:"7vw"}}>Submit </button>
+    </div>
+  )}
 
+  
+
+  
 export default App
