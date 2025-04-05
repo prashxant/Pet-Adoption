ecae4b62bacd0977a50b8cdb67faf708b0673b93
prashxant <notxdead0@gmail.com>
2025-04-05 13:09:56 +0530

    completed

--- pet-adoption/src/App.jsx
+++ pet-adoption/src/App.jsx
@@ -1,33 +1,76 @@
-
-
+import React, { useState } from "react";
 
 function App() {
-const header = ["Name", "Email", "Phone", "Pet Name", "Type", "Breed"]
+  const headers = ["Name", "Email", "Phone", "Pet Name", "Type", "Breed"];
 
-return (<>
-<div>
-{
-  header.map(function(header,index){
-    return (
-      <div key = {index}>
-            <h1>{header}</h1>
-            <input type="text" />
+  const [formData, setFormData] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  const handleChange = (e, header) => {
+    setFormData({
+      ...formData,
+      [header]: e.target.value,
+    });
+  };
+
+  const submit = () => {
+    setTableData([...tableData, formData]);
+    setFormData({}); 
+  };
+
+  return (
+    <>
+     
+      <div>
+        {headers.map((header, index) => (
+          <div key={index}>
+            <h5>{header}</h5>
+            <input
+              type="text"
+              value={formData[header] || ""}
+              onChange={(e) => handleChange(e, header)}
+            />
+          </div>
+        ))}
       </div>
-    )
 
-  })
+      
+      <div style={{ paddingLeft: "5vw", marginTop: "2vh" }}>
+        <button
+          style={{
+            backgroundColor: "blue",
+            color: "white",
+            borderRadius: "5px",
+            height: "30px",
+            width: "100px",
+          }}
+          onClick={submit}
+        >
+          Submit
+        </button>
+      </div>
+
+   
+      <table border="1" style={{ marginTop: "20px", width: "80%" }}>
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {headers.map((header, index) => (
+                <td key={index}>{row[header] || "-"}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
-</div>
-<div> <button onClick={()=>(alert("Sunmit"))}><h1>Submit</h1></button></div>
-</>
-)
 
-
-
-
-
-
-
-
-}
-export default App
+export default App;
